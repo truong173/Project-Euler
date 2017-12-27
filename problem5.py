68e9f0cf1b11f22d06ce607fe27bebd36157e696
Truong Hoang <truong173@users.noreply.github.com>
2017-12-27 22:24:05 +0700

    turn the solution into a function lcm(num)

--- problem5.py
+++ problem5.py
@@ -8,46 +8,45 @@
 
 # WORKING SOLUTION FINALLY!
 
-#input number
-max_num = int(input("small number pls: "))
+def lcm(max_num):
+    
+    #define a function to get the product of all value of an input list
+    def product(input_list):
+        p = 1
+        for factor in input_list:
+            p *= factor
+        return p
 
-#define a function to get the product of all value of an input list
-def product(input_list):
-    p = 1
-    for il in input_list:
-        p *= il
-    return p
+    #get the true list from 11 to 21
+    num_list = range(2, max_num + 1)
 
-#get the true list from 11 to 21
-true_list = range(2, max_num + 1)
+    #separate the primes from the true list
+    primes = [x for x in num_list if all(x % y != 0 for y in range(2, x))]
 
-#separate the primes from the true list
-primes = [x for x in true_list if all(x % y != 0 for y in range(2, x))]
+    #separate the non-prime numbers from the true list
+    non_primes = [z for z in num_list if z not in primes]
 
-#separate the non-prime numbers from the true list
-rest = [z for z in true_list if z not in primes]
+    divs = []
 
-divs = []
+    for np1 in non_primes:
+        if product(primes) % np1 != 0:
+            divs = divs + [div for div in primes if np1 % div == 0 and div not in divs]
 
-for r in rest:
-    if product(primes) % r != 0:
-        divs = divs + [div for div in primes if r % div == 0 and div not in divs]
+    multi = 1
 
-multi = 1
-
-for d in divs:
-    for r in rest:
-        for power in range(1, 10):
-            if ( product(primes) * multi ) % r == 0:
-                break
-            else:
-                if r < d ** power:
-                    for reverse in range(1, power):
-                        multi = int(multi / (d ** reverse))
+    for d in divs:
+        for np2 in non_primes:
+            for power in range(1, 10):
+                if ( product(primes) * multi ) % np2 == 0:
                     break
                 else:
-                    multi = multi * (d ** power)
-                    pass
+                    if np2 < d ** power:
+                        for reverse in range(1, power):
+                            multi = int(multi / (d ** reverse))
+                        break
+                    else:
+                        multi = multi * (d ** power)
+                        pass
 
-result = product(primes) * multi
-print(result)
+    result = product(primes) * multi
+    return result
