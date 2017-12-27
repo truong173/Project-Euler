3c3a615173a62853749265099465f06ac0aac058
Truong Hoang <truong173@users.noreply.github.com>
2017-12-27 22:16:24 +0700

    final solution to problem5

--- problem5.py
+++ problem5.py
@@ -6,10 +6,10 @@
 
 #This feels like a shitty solution, because I'm not certain about my algorithm yet, but I'm gonn keep tinkering with it for a while
 
-#UNFINISHED SOLUTION! STILL WORKING ON THE PROBLEM.
+# WORKING SOLUTION FINALLY!
 
 #input number
-max_num = 10
+max_num = int(input("small number pls: "))
 
 #define a function to get the product of all value of an input list
 def product(input_list):
@@ -23,24 +23,31 @@ true_list = range(2, max_num + 1)
 
 #separate the primes from the true list
 primes = [x for x in true_list if all(x % y != 0 for y in range(2, x))]
-primes_origin = [x for x in true_list if all(x % y != 0 for y in range(2, x))]
-print("primes: ", primes, product(primes))
+
 #separate the non-prime numbers from the true list
 rest = [z for z in true_list if z not in primes]
-print("rest: ", rest)
 
 divs = []
-divs2 = []
+
 for r in rest:
-	if product(primes) % r != 0:
-		divs = divs + [div for div in primes if r % div == 0 and div not in divs]
-print(divs)
+    if product(primes) % r != 0:
+        divs = divs + [div for div in primes if r % div == 0 and div not in divs]
+
+multi = 1
+
 for d in divs:
-	for r in rest:
-		for power in range(1, 7):
-			if ( product(primes) * (d ** power) ) % r == 0:
-				print(power)
-				break
-			else:
-				pass
-			
+    for r in rest:
+        for power in range(1, 10):
+            if ( product(primes) * multi ) % r == 0:
+                break
+            else:
+                if r < d ** power:
+                    for reverse in range(1, power):
+                        multi = int(multi / (d ** reverse))
+                    break
+                else:
+                    multi = multi * (d ** power)
+                    pass
+
+result = product(primes) * multi
+print(result)
